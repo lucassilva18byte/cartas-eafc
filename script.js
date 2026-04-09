8ac27ef84af8cd8291fdf2b252b55192a4ecc0ce
lucassilva18byte <lucas.santossilva1906@gmail.com>
2026-04-09 21:22:59 +0000

    teste 2

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-function abrirCarta() {
+function abrirPack() {
+  const pack = document.getElementById("pack");
   const carta = document.getElementById("carta");
 
-  // reset
-  carta.classList.remove("show");
-  carta.classList.add("hidden");
+  // animação do pack
+  pack.classList.add("open");
 
-  // flash tipo FIFA
+  // flash estilo FIFA
   document.body.classList.add("flash");
   setTimeout(() => {
     document.body.classList.remove("flash");
   }, 200);
 
-  // suspense
+  // depois de abrir
   setTimeout(() => {
+    pack.style.display = "none";
+
     carta.classList.remove("hidden");
 
     setTimeout(() => {
       carta.classList.add("show");
     }, 100);
 
-  }, 800);
+  }, 600);
 }
